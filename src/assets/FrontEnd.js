0acfe20f21e6c4e32f6ccb9550d21596e941520c
Ankit-Mishra2025 <devoankit2025@gmail.com>
2025-08-01 16:42:41 +0530

    File name error

--- src/assets/FrontEnd.js
+++ src/assets/FrontEnd.js
@@ -1,61 +1,53 @@
+import html from "../assets/html.svg";
+import css from "../assets/css.png";
+import js from "../assets/js.svg";
+import ts from "../assets/tailwind.svg";
 
-import html from '../assets/html.svg'
-import css from '../assets/css.png'
-import js from '../assets/js.svg'
-import ts from '../assets/tailwind.svg'
+import react from "../assets/Reactt.svg";
+import shadcn from "../assets/shadnc.png";
+import framer from "../assets/framer.png";
+import Api from "../assets/Api.png";
 
-import react from '../assets/reactt.svg'
-import shadcn from '../assets/shadnc.png'
-import framer from '../assets/framer.png'
-import Api from '../assets/Api.png'
+const Frontend = [
+  {
+    s_no: "1",
+    s_img: html,
+  }, 
 
-const Frontend=[{
-    s_no:"1",
-    s_img: html
-},
+  {
+    s_no: "2",
+    s_img: css,
+  },
 
-{
-s_no:"2",
-    s_img:css
+  {
+    s_no: "3",
+    s_img: js,
+  },
 
-},
+  {
+    s_no: "4",
+    s_img: ts,
+  },
 
-{
-    s_no:"3",
-    s_img:js
-},
+  {
+    s_no: "5",
+    s_img: react,
+  },
 
+  {
+    s_no: "6",
+    s_img: shadcn,
+  },
 
-{
-    s_no:"4",
-    s_img:ts
-},
+  {
+    s_no: "7",
+    s_img: framer,
+  },
 
+  {
+    s_no: "8",
+    s_img: Api,
+  },
+];
 
-
-
-{
-    s_no:"5",
-    s_img:react
-},
-
-
-
-{
-    s_no:"6",
-    s_img:shadcn
-},
-
-{
-    s_no:"7",
-    s_img:framer
-},
-
-{
-    s_no:"8",
-    s_img:Api
-},
-
-]
-
-export default Frontend
+export default Frontend;
