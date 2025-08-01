d98ae9e4e15423d18fa4137658a42c9242e5b0f5
Ankit-Mishra2025 <devoankit2025@gmail.com>
2025-08-01 16:44:25 +0530

    File error

--- src/assets/ProjectData.js
+++ src/assets/ProjectData.js
@@ -1,64 +1,65 @@
-import Gemini from "../assets/Gemini.png"
-import Portal from "../assets/Job Portal.png"
-import Netflix from "../assets/Netflix.png"
-import movie from "../assets/Movie.png"
-import watch from "../assets/DigitalWatch.png"
-import imageSearch from "../assets/Image search.png"
+import Gemini from "../assets/Gemini.png";
+import Portal from "../assets/Job Portal.png";
+import Netflix from "../assets/Netflix.png";
+import movie from "../assets/Movie.png";
+import watch from "../assets/Digitalwatch.png";
+import imageSearch from "../assets/Image search.png";
 
-const Project_Data=[{
-    s_no:"01",
+const Project_Data = [
+  {
+    s_no: "01",
 
-    s_name:"Google Gemini",
-    s_img:Gemini,
-    s_desc:"Build google gemini replica using React js ,tailwind css and google gemini api.It show the outpu on user questions. It helped me to enhance my API intergration skill."
+    s_name: "Google Gemini",
+    s_img: Gemini,
+    s_desc:
+      "Build google gemini replica using React js ,tailwind css and google gemini api.It show the outpu on user questions. It helped me to enhance my API intergration skill.",
+  },
 
+  {
+    s_no: "02",
+    s_name: "JOB PORTAL",
+    s_img: Portal,
+    s_desc:
+      "Designed and code a full stack job portal with added features like admin panel,applicants panel by using MERN.",
+    s_git: "https://github.com/Ankit-Mishra2025/Mern_Job_Portal",
+  },
 
-},
+  {
+    s_no: "03",
+    s_name: "Netflix Clone",
+    s_img: Netflix,
+    s_desc:
+      "Designed a fully responsive Netflix clone to sharpen my front-end skill by using react js ,tailwind css for better UI and Firebase for authentication",
+    s_git: "https://github.com/Ankit-Mishra2025/Netflix_Clone",
+  },
 
-{
-    s_no:"02",
-    s_name:"JOB PORTAL",
-    s_img:Portal,
-    s_desc:"Designed and code a full stack job portal with added features like admin panel,applicants panel by using MERN.",
-    s_git:"https://github.com/Ankit-Mishra2025/Mern_Job_Portal"
+  {
+    s_no: "04",
+    s_name: "Movie Website",
+    s_img: movie,
+    s_desc:
+      "Created and design a Movie website by using HTML,CSS and Javascript",
+    s_git:
+      "https://github.com/Ankit-Mishra2025/Javascript_Mini_Projects_Practice/tree/main/Movie%20Website",
+  },
 
-},
+  {
+    s_no: "05",
+    s_name: "Digital Watch",
+    s_img: watch,
+    s_desc:
+      "Create a digital watch during my front-end development journey to deep dive in javascript concepts",
+    s_git:
+      "https://github.com/Ankit-Mishra2025/Javascript_Mini_Projects_Practice/tree/main/Analog%20Watch",
+  },
 
-
-{
-     s_no:"03",
-    s_name:"Netflix Clone",
-    s_img:Netflix,
-    s_desc:"Designed a fully responsive Netflix clone to sharpen my front-end skill by using react js ,tailwind css for better UI and Firebase for authentication",
-    s_git:"https://github.com/Ankit-Mishra2025/Netflix_Clone"
-},
-
-
-
-{
-     s_no:"04",
-    s_name:"Movie Website",
-    s_img:movie,
-    s_desc:"Created and design a Movie website by using HTML,CSS and Javascript",
-    s_git:"https://github.com/Ankit-Mishra2025/Javascript_Mini_Projects_Practice/tree/main/Movie%20Website"
-},
-
-{
-      s_no:"05",
-    s_name:"Digital Watch",
-    s_img:watch,
-    s_desc:"Create a digital watch during my front-end development journey to deep dive in javascript concepts",
-    s_git:"https://github.com/Ankit-Mishra2025/Javascript_Mini_Projects_Practice/tree/main/Analog%20Watch"
-},
-
-{
-      s_no:"06",
-    s_name:"Image Search Engine",
-    s_img:imageSearch,
-    s_desc:"In this project i have created a image search engine which was assigned to me during my internship project by using Javascript and pexels API",
-    s_git:""
-
-}
-
-]
-export default  Project_Data
+  {
+    s_no: "06",
+    s_name: "Image Search Engine",
+    s_img: imageSearch,
+    s_desc:
+      "In this project i have created a image search engine which was assigned to me during my internship project by using Javascript and pexels API",
+    s_git: "",
+  },
+];
+export default Project_Data;
